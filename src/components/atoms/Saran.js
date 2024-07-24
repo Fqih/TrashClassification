c32017344ff1f52b685a019f3ab610eada93e546
fqih <mhmdfkih21@gmail.com>
2024-07-24 21:10:45 +0700

    2

--- src/components/atoms/Saran.js
+++ src/components/atoms/Saran.js
@@ -1,14 +1,14 @@
 export const saran = {
     battery: 'Harap buang baterai di pusat daur ulang baterai yang ditunjuk.',
-    biological: 'Sampah biologis harus dikomposkan atau dibuang di tempat sampah organik khusus.',
-    'brown-glass': 'Harap daur ulang botol dan jar kaca coklat di tempat daur ulang kaca yang ditunjuk.',
-    cardboard: 'Kardus harus diratakan dan ditempatkan di tempat daur ulang kertas.',
-    clothes: 'Donasikan atau daur ulang pakaian di tempat daur ulang tekstil.',
-    'green-glass': 'Harap daur ulang botol dan jar kaca hijau di tempat daur ulang kaca yang ditunjuk.',
+    biological: 'Sampah biologis harus dijadikan kompos atau dibuang di tempat sampah organik',
+    'brown-glass': 'Harap daur ulang botol kaca coklat di tempat daur ulang kaca dan hati hati',
+    cardboard: 'Kardus harus ditempatkan di tempat daur ulang kertas.',
+    clothes: 'Donasikan atau daur ulang pakaian di tempat daur ulang.',
+    'green-glass': 'Harap daur ulang botol kaca hijau di tempat daur ulang kaca dan hati hati',
     metal: 'Buang kaleng logam dan logam bekas di tempat daur ulang logam.',
-    paper: 'Kertas harus didaur ulang di tempat daur ulang kertas.',
-    plastic: 'Buang plastik di tempat daur ulang plastik.',
-    shoes: 'Donasikan sepatu lama ke badan amal atau daur ulang di fasilitas khusus.',
+    paper: 'Kertas bisa didaur ulang di tempat daur ulang',
+    plastic: 'Buang plastik di tempat daur ulang atau ubah jadi kerajinan',
+    shoes: 'Donasikan sepatu lama ke badan amal atau daur ulang.',
     trash: 'Buang di tempat sampah umum.',
-    'white-glass': 'Harap daur ulang botol dan jar kaca putih di tempat daur ulang kaca yang ditunjuk.'
+    'white-glass': 'Harap daur ulang botol kaca putih di tempat daur ulang kaca dan hati hati'
 };
